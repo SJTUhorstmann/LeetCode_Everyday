9e69e81d28f775f45bea8578334281a0bd0c12c7
aogutiancheng <18701987235@163.com>
2019-06-10 17:11:32 +0800

    add topic680 from chenkui

---
+++ topic680_回文字符串/from chenkui.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j && s.charAt(i) == s.charAt(j)) {
+            i++; j--;
+        }
+        if (i >= j) return true;
+        if (isPalin(s, i + 1, j) || isPalin(s, i, j - 1)) return true;
+        return false;
+    }
+    private boolean isPalin(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++; j--;
+            }
+            else return false;
+        }
+        return true;
+    }
+}
---
+++ topic680_回文字符串/readme.txt
@@ -0,0 +1,11 @@
+﻿题目描述：可以删除一个字符，判断是否能构成回文字符串。
+
+Example 1:
+Input: "aba"
+Output: True
+
+
+Example 2:
+
+Input: "abca"
+Output: True
