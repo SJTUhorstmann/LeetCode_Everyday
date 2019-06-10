2ace5d5655a3b6ea23c49a32c504a4069573d420
aogutiancheng <18701987235@163.com>
2019-06-10 18:12:15 +0800

    add topic524 from chenkui

---
+++ topic524_最长子序列/from chenkui.java
@@ -0,0 +1,28 @@
+﻿/**
+ * Definition for singly-linked list.
+class Solution {
+    public String findLongestWord(String s, List<String> d) {
+        String longestWord = "";
+        for (String target : d) {
+            int l1 = longestWord.length(), l2 = target.length();
+            if (l1 > l2 || (l1 == l2 && longestWord.compareTo(target) < 0)) {
+                continue;
+            }
+            if (isSubstr(s, target)) {
+                longestWord = target;
+            }
+        }
+        return longestWord;
+    }
+
+    private boolean isSubstr(String s, String target) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < target.length()) {
+            if (s.charAt(i) == target.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        return j == target.length();
+    }
+}
---
+++ topic524_最长子序列/readme.txt
@@ -0,0 +1,14 @@
+﻿题目描述：删除 s 中的一些字符，使得它构成字符串列表 d 中的一个字符串，找出能构成的最长字符串。如果有多个相同长度的结果，返回字典序的最小字符串。
+
+Example 1:
+Input:
+s = "abpcplea", d = ["ale","apple","monkey","plea"]
+Output: 
+"apple"
+
+Example 2:
+Input:
+s = "abpcplea", d = ["a","b","c"]
+
+Output: 
+"a"
