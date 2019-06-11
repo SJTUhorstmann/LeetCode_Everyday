191e15e1cd827a2db50227e9dccc1e41391e7944
yangpf <yangpf0810@sjtu.edu.cn>
2019-06-11 09:24:48 +0800

    add topic1 solution by yangpf

--- topic1_two_sum/from yangpf.java
+++ topic1_two_sum/from yangpf.java
@@ -14,3 +14,22 @@ class Solution {
         return array;
     }
 }
+
+//暴力解法
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        if(n<=1 || nums==null)  return null;
+        int[] res=new int[2];
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    res[0]=i;
+                    res[1]=j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+}
