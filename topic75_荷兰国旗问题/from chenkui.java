35eb7178128e84bacc6fb77fb7b0567d8f137e57
aogutiancheng <18701987235@163.com>
2019-06-11 09:31:41 +0800

    add topic75 from chenkui

---
+++ topic75_荷兰国旗问题/from chenkui.java
@@ -0,0 +1,26 @@
+﻿class Solution {
+    public void sortColors(int[] nums) {
+      int left=-1;
+      int mid=0;
+      int right=nums.length;
+      while(mid<right){
+        if(nums[mid]==0){
+          swap(nums,++left,mid++);
+        }
+        else if(nums[mid]==2)
+        {
+          swap(nums,mid,--right);
+        }
+        else{
+          mid++;
+        }
+      }   
+    }
+    public void swap(int[] nums,int a,int b){
+      int tem=nums[a];
+      nums[a]=nums[b];
+      nums[b]=tem;
+    }
+}
+ 
+}
