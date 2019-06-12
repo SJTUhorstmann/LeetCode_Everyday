7ec5047116f1f0984e3a5d343460693145962020
aogutiancheng <18701987235@163.com>
2019-06-12 19:31:45 +0800

    add topic455 by chenkui

---
+++ chenkui/topic455_分配饼干/from chenkui.java
@@ -0,0 +1,17 @@
+﻿class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+      Arrays.sort(g);
+      Arrays.sort(s);
+      int i=0;
+      int j=0;
+      int count=0;
+      while(i<g.length && j<s.length){
+        if(g[i]<=s[j]){
+          count++;
+          i++;
+        }
+          j++;
+      }
+      return count;  
+    }
+}
