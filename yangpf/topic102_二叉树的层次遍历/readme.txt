782f3c626c86a3aa91f5cf3c38cc6b19ade9eba0
yangpf <yangpf0810@sjtu.edu.cn>
2019-06-12 20:24:07 +0800

    add topic 102

---
+++ yangpf/topic102_二叉树的层次遍历/readme.txt
@@ -0,0 +1,21 @@
+给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+注意：
+List<List<Integer>> res=new ArrayList<>();  没有问题
+但是   List<List<Integer>> res=new ArrayList<ArrayList<Integer>>(); 会出现ArrayList<ArrayList<Integer>> cannot be converted to List<List<Integer>>的报错
---
+++ yangpf/topic102_二叉树的层次遍历/队列实现.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null)  return res;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            ArrayList<Integer> list=new ArrayList<>();
+            while(size-->0){
+                TreeNode cur=q.poll();
+                list.add(cur.val);
+                if(cur.left!=null)
+                    q.offer(cur.left);
+                if(cur.right!=null)
+                    q.offer(cur.right);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
