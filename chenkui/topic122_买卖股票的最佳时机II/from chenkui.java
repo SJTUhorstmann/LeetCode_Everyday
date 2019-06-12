94a7d8d113751c225ee169b3c12ccd0bdca86ea8
aogutiancheng <18701987235@163.com>
2019-06-12 19:55:59 +0800

    add topic122 by chenkui

---
+++ chenkui/topic122_买卖股票的最佳时机II/from chenkui.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+      if(prices==null || prices.length<2) return 0;
+      int proift=0;
+      for(int i=1;i<prices.length;i++){
+        if(prices[i]>prices[i-1])
+          proift=proift+prices[i]-prices[i-1];
+      }
+        return proift;
+    }
+}
+
