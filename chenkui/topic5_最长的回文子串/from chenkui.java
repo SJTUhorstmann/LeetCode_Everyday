938f274123b035e58409c03b2829a5629fc12da0
chenkui <aogutiancheng>
2019-06-22 23:57:45 +0800

    add topic5 by chenkui

---
+++ chenkui/topic5_最长的回文子串/from chenkui.java
@@ -0,0 +1,27 @@
+﻿class Solution {
+    int maxLen=0;
+    int left=0;
+    
+    public String longestPalindrome(String s) {
+      int len=s.length();
+      if(len<2) return s;
+      for(int i=0;i<len-1;i++){
+        Palindrome(s,i,i);
+        Palindrome(s,i,i+1);
+      }
+      
+      return s.substring(left,left+maxLen);
+    }
+    public void Palindrome(String s,int begin,int end){
+       while(begin>=0 && end <s.length() && s.charAt(begin)==s.charAt(end)){
+        begin--;
+        end++;
+      }
+      if(maxLen<end-begin-1){
+        left=begin+1;
+        maxLen=end-begin-1;
+      }
+      
+    }
+   
+}
