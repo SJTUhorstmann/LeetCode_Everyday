07bec6bb60063690ac4f1c2dd782587903e8d73b
chenkui <aogutiancheng>
2019-06-23 00:32:22 +0800

    add topic322 by chenkui

---
+++ chenkui/topic322_硬币变化/from chenkui.java
@@ -0,0 +1,21 @@
+﻿class Solution {
+        public static int coinChange(int[] coins, int amount) {
+
+        if (coins == null || coins.length == 0 || amount <= 0)
+            return 0;
+        int[] minNumber = new int[amount + 1];
+        Arrays.fill(minNumber, Integer.MAX_VALUE);
+        minNumber[0] =0;
+       
+        for(int coin : coins) {
+            for (int i = 1; i <= amount; i++) {
+                if (coin <= i && minNumber[i - coin] != Integer.MAX_VALUE)
+                    minNumber[i] = Integer.min(minNumber[i], 1 + minNumber[i - coin]);
+            }
+        }
+        if (minNumber[amount] == Integer.MAX_VALUE)
+            return -1;
+        else
+            return minNumber[amount];
+    }
+}
---
+++ chenkui/topic322_硬币变化/readme.txt
@@ -0,0 +1,12 @@
+﻿题目描述：给定不同面额的硬币和总金额。 编写一个函数来计算构成该数量所需的最少数量的硬币。 如果这笔钱不能由任何硬币组合弥补，则返回-1。
+
+Example 1:
+
+Input: coins = [1, 2, 5], amount = 11
+Output: 3 
+Explanation: 11 = 5 + 5 + 1
+
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
