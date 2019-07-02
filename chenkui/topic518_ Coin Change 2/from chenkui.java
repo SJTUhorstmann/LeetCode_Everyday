ccdf9025458726db6856c078a6835c4a14dbe9c5
chenkui <aogutiancheng>
2019-07-02 23:21:00 +0800

    add 518 by chenkui

---
+++ chenkui/topic518_ Coin Change 2/from chenkui.java
@@ -0,0 +1,18 @@
+﻿class Solution {
+    public int change(int amount, int[] coins) {
+        if(coins==null || coins.length==0)
+           return 0;
+        if(amount==0 && ) return 1;
+        int[] dp=new int[amount+1];
+        dp[0]=1;
+        
+        for(int i=0;i<coins.length;i++){
+            int tem=coins[i];
+            for(int j=tem;j<=amount;j++){
+                dp[j]=dp[j]+dp[j-tem];
+            }
+        }
+        return dp[amount];
+}
+
+}
---
+++ chenkui/topic518_ Coin Change 2/readme.txt
@@ -0,0 +1,17 @@
+﻿题目描述：给定不同面额和总金额的硬币。 编写一个函数来计算构成该数量的组合数。 你可以假设你拥有无限数量的每种硬币。
+
+Example 1:
+
+Input: amount = 5, coins = [1, 2, 5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+
+Example 2:
+
+Input: amount = 3, coins = [2]
+Output: 0
+Explanation: the amount of 3 cannot be made up just with coins of 2.
