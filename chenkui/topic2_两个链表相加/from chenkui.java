76d092888a8c29c381229da388ded23725eea75d
aogutiancheng <18701987235@163.com>
2019-07-03 21:58:54 +0800

    modify 2 by chenkui

--- chenkui/topic2_两个链表相加/from chenkui.java
+++ chenkui/topic2_两个链表相加/from chenkui.java
@@ -1,23 +1,24 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-      int add=0;
-      ListNode list=new ListNode(0);
-      ListNode list1=list;
-      while(l1!=null || l2!=null || add!=0){
-        if(l1!=null){
-          add=add+l1.val;
-          l1=l1.next;
-        }
-        if(l2!=null){
-          add=add+l2.val;
-          l2=l2.next;
-        }
-        list.next=new ListNode(add%10);
-        add=add/10;
-        list=list.next;
-      }
+        return add(l1, l2, 0);
+    }
 
-      return list1.next;
-      
+    private ListNode add(ListNode l1, ListNode l2, int carry) {
+        int val1 = l1 != null ? l1.val : 0;
+        int val2 = l2 != null ? l2.val : 0;
+
+        if (l1 == null && l2 == null) {
+            return carry == 0 ? null : new ListNode(carry); 
+        }
+
+        int result = val1 + val2 + carry;
+        int newCarry = 0;
+        if (result >= 10) newCarry = 1;
+        ListNode res = new ListNode(result % 10);
+        ListNode nextL1 = l1 != null ? l1.next : null;
+        ListNode nextL2 = l2 != null ? l2.next : null;
+        res.next = add(nextL1, nextL2, newCarry);
+
+        return res;
     }
 }
