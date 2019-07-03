e0b6570f455ccc6fe28995c6490be7122809f2ad
aogutiancheng <18701987235@163.com>
2019-07-03 21:52:37 +0800

    modify 518 by chenkui

--- chenkui/topic518_ Coin Change 2/from chenkui.java
+++ chenkui/topic518_ Coin Change 2/from chenkui.java
@@ -1,18 +1,20 @@
 ﻿class Solution {
     public int change(int amount, int[] coins) {
-        if(coins==null || coins.length==0)
-           return 0;
-        if(amount==0 && ) return 1;
-        int[] dp=new int[amount+1];
-        dp[0]=1;
-        
-        for(int i=0;i<coins.length;i++){
-            int tem=coins[i];
-            for(int j=tem;j<=amount;j++){
-                dp[j]=dp[j]+dp[j-tem];
+
+        if(amount <=0) return 1;
+        if(amount>0 && coins.length<1) return 0;
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+
+        for(int j = 0; j < coins.length; j++ ) {
+            for(int i = 1; i < dp.length; i++) {
+                if(i - coins[j] >=0) {
+                    int subamount = i - coins[j];
+                    dp[i] += dp[subamount];
+                }
             }
         }
-        return dp[amount];
-}
 
+        return dp[dp.length-1];
+    }
 }
