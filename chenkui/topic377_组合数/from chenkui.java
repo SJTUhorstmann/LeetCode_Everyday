a75b3449d5b6dbc07376370b312752a24b4969f6
aogutiancheng <18701987235@163.com>
2019-07-03 21:43:49 +0800

    modify 377 by chenkui

--- chenkui/topic377_组合数/from chenkui.java
+++ chenkui/topic377_组合数/from chenkui.java
@@ -1,18 +1,20 @@
 ﻿class Solution {
    public int combinationSum4(int[] nums, int target) {
+
     if (nums == null || nums.length == 0) {
         return 0;
     }
-       
-    int[] dp = new int[target + 1];
-    dp[0] = 1;
+
+    int[] maximum = new int[target + 1];
+    maximum[0] = 1;
     Arrays.sort(nums);
-       
+
     for (int i = 1; i <= target; i++) {
         for (int j = 0; j < nums.length && nums[j] <= i; j++) {
-            dp[i] += dp[i - nums[j]];
+            maximum[i] += maximum[i - nums[j]];
         }
     }
-    return dp[target];
+
+    return maximum[target];
 }
 }
