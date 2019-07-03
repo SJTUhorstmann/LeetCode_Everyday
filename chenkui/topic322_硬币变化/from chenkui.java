8bab3b8752c4899b5dc9a985498aad0717f90643
aogutiancheng <18701987235@163.com>
2019-07-03 22:07:33 +0800

    modify 322 by chenkui

--- chenkui/topic322_硬币变化/from chenkui.java
+++ chenkui/topic322_硬币变化/from chenkui.java
@@ -3,6 +3,7 @@
 
         if (coins == null || coins.length == 0 || amount <= 0)
             return 0;
+            
         int[] minNumber = new int[amount + 1];
         Arrays.fill(minNumber, Integer.MAX_VALUE);
         minNumber[0] =0;
@@ -13,6 +14,7 @@
                     minNumber[i] = Integer.min(minNumber[i], 1 + minNumber[i - coin]);
             }
         }
+            
         if (minNumber[amount] == Integer.MAX_VALUE)
             return -1;
         else
