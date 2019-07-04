2cd15d2d703e407ef83da6f3f45fd50bbed15e2a
aogutiancheng <18701987235@163.com>
2019-07-04 23:49:35 +0800

    add 405 by chenkui

---
+++ chenkui/topic405_转换成十六进制/from chenkui.java
@@ -0,0 +1,15 @@
+﻿class Solution {
+   public String toHex(int num) {
+    char[] arr = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+       
+    if (num == 0) return "0";
+    StringBuilder sb = new StringBuilder();
+       
+    while (num != 0) {
+        sb.append(arr[num & 0b1111]);
+        num >>>= 4;
+    }
+       
+    return sb.reverse().toString();
+}
+}
