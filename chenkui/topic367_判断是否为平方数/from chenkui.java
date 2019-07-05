635bf9f810d0e865aeb231f88e703a160e94cd3e
aogutiancheng <18701987235@163.com>
2019-07-05 23:31:51 +0800

    add 367 by chenkui

---
+++ chenkui/topic367_判断是否为平方数/from chenkui.java
@@ -0,0 +1,9 @@
+﻿class Solution {
+    public boolean isPerfectSquare(int num) {
+    
+        int tem=(int)(Math.sqrt(num));
+        
+        return num==tem*tem;
+        
+    }
+}
---
+++ chenkui/topic367_判断是否为平方数/readme.txt
@@ -0,0 +1,10 @@
+﻿题目描述：给定一个整数，判断是否为平方数。
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 14
+Output: false
