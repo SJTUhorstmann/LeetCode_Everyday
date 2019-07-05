82b75352403d3ffe9a162c2a42e941566e52db09
aogutiancheng <18701987235@163.com>
2019-07-05 23:46:31 +0800

    add 238 by chenkui

---
+++ chenkui/topic238_除自身外的矩阵其他元素之积/from chenkui.java
@@ -0,0 +1,22 @@
+﻿class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int len = nums.length;
+        int[] arr = new int[len];
+        Arrays.fill(arr, 1);
+        
+        int left = 1;
+        for (int i = 1; i < len; i++) {
+           left *= nums[i - 1];
+           arr[i] *= left;
+        }
+        
+        int right = 1;
+        for (int i = len - 2; i >= 0; i--) {
+        right *= nums[i + 1];
+        arr[i] *= right;
+        }
+        
+    return arr;
+}
+}
