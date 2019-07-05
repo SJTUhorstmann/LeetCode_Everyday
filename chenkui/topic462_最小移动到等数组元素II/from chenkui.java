846aa5f959a0eee7d6c63be154dd7637b5627721
aogutiancheng <18701987235@163.com>
2019-07-05 23:08:33 +0800

    modify 415 by chenkui

---
+++ chenkui/topic462_最小移动到等数组元素II/from chenkui.java
@@ -0,0 +1,18 @@
+﻿class Solution {
+    public int minMoves2(int[] nums) {
+        
+        if(nums.length<=1) return 0;
+        
+        int move=0;
+        int left=0;
+        int right=nums.length-1;
+        Arrays.sort(nums);
+        
+        while(left<right){
+            move=move+nums[right]-nums[left];
+            right--;
+            left++;
+        }
+        return move;
+    }
+}
