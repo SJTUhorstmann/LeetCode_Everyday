ffede794ed28328874cea5dc3e20aa563d157f1b
aogutiancheng <18701987235@163.com>
2019-07-07 12:29:14 +0800

    add 144_IP by chenkui

---
+++ chenkui/topic144_IP 地址无效化/from chenkui.java
@@ -0,0 +1,15 @@
+﻿class Solution {
+    public String defangIPaddr(String address) {
+        if(address==null )
+            return "";
+        String sb="";
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)!='.'){
+                sb=sb+address.charAt(i);
+            }
+            else{
+                sb=sb+"[.]";
+            }
+        }
+       return sb; 
+    }
