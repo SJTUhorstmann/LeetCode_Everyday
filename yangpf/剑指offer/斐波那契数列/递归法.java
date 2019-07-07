4c3d4742168c0928c22e137e54ac9aa3a3b51212
yangpf <yangpf0810@sjtu.edu.cn>
2019-07-07 09:53:07 +0800

    斐波那契数列

---
+++ yangpf/剑指offer/斐波那契数列/递归法.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int Fibonacci(int n) {
+        if(n==0)    return 0;
+        if(n==1||n==2)    return 1;
+        return Fibonacci(n-1)+Fibonacci(n-2);
+    }
+}
