1c050a6d3be81063be3082f22981ce0d27a1ef2f
aogutiancheng <18701987235@163.com>
2019-07-07 12:31:18 +0800

    add 144_航班 by chenkui

---
+++ chenkui/topic144_航班预订统计/from chenkui.java
@@ -0,0 +1,11 @@
+﻿class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] res = new int[n];
+        for (int[] booking : bookings) {
+            for (int i = booking[0] - 1; i <= booking[1] - 1; i++) {
+                res[i] += booking[2];
+            }
+        }
+        return res;
+    }
+}
