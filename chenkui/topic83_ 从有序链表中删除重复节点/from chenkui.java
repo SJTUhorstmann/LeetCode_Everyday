8b26ae7ac08e7c6eaf727bc10afd90618e96c217
aogutiancheng <18701987235@163.com>
2019-07-09 22:19:34 +0800

    add 83 by chenkui

---
+++ chenkui/topic83_ 从有序链表中删除重复节点/from chenkui.java
@@ -0,0 +1,31 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        
+        ListNode result=head;
+        while(head!=null){
+            
+            int tem=head.val;
+            ListNode head1=head.next;
+            while (head1!=null && head1.val==tem){
+                head1=head1.next;
+            }
+            
+            head.next=head1;
+            head=head.next;
+            
+        }
+        return result;
+            
+    }
+}
