2768819da2770e4b7de80a4e964065bf2391dd8a
aogutiancheng <18701987235@163.com>
2019-07-09 23:18:44 +0800

    modify topic19 by chenkui

--- chenkui/topic19_从列表末尾删除第N个节点/from chenkui.java
+++ chenkui/topic19_从列表末尾删除第N个节点/from chenkui.java
@@ -1,5 +1,14 @@
-﻿class Solution {
-     public ListNode removeNthFromEnd(ListNode head, int n) {
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    //方法1
+     /*public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode prev = null;
         ListNode slowPointer = head;
         ListNode fastPointer = head;
@@ -7,15 +16,38 @@
         while(fastPointer != null) {
             if (n != 0) { 
                 n--;
-            } else {
+            } 
+            else {
                 prev = slowPointer;
                 slowPointer = slowPointer.next;                
             }
             fastPointer = fastPointer.next;
         }
         
-        if (prev == null) return slowPointer.next;
+        if (prev == null)
+            return slowPointer.next;
+         
         prev.next = slowPointer.next;
         return head;
+    }*/
+    
+    //方法2
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode fast = head;
+        
+    while (n-- > 0) {
+        fast = fast.next;
     }
+        
+    if (fast == null) 
+        return head.next;
+        
+    ListNode slow = head;
+    while (fast.next != null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    return head;
+}
 }
