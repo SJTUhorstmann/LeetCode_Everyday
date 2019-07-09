10ca55a5de4101c749a847c2f96f85ee5c4e00fb
aogutiancheng <18701987235@163.com>
2019-07-09 23:37:14 +0800

    add topic24 by chenkui

---
+++ chenkui/topic24_交换链表中的相邻结点/from chenkui.java
@@ -0,0 +1,21 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode next=head.next.next;
+        ListNode node=head.next;
+        node.next=head;
+        node.next.next=swapPairs(next);
+        return node;
+        
+    }
+}
