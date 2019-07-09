408b1957c865d220ad95f8a7df6bbaec71289382
aogutiancheng <18701987235@163.com>
2019-07-09 23:38:48 +0800

    modify topic24 by chenkui

--- chenkui/topic24_交换链表中的相邻结点/from chenkui.java
+++ chenkui/topic24_交换链表中的相邻结点/from chenkui.java
@@ -7,6 +7,7 @@
  * }
  */
 class Solution {
+    //方法1
     public ListNode swapPairs(ListNode head) {
         if(head==null || head.next==null){
             return head;
@@ -18,4 +19,22 @@ class Solution {
         return node;
         
     }
+
+   //方法2
+
+   public ListNode swapPairs(ListNode head) {
+    ListNode node = new ListNode(-1);
+    node.next = head;
+    ListNode pre = node;
+    while (pre.next != null && pre.next.next != null) {
+        ListNode l1 = pre.next, l2 = pre.next.next;
+        ListNode next = l2.next;
+        l1.next = next;
+        l2.next = l1;
+        pre.next = l2;
+
+        pre = l1;
+    }
+    return node.next;
+}
 }
