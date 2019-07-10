86b7cbac6c63e9c790ab097d64721d52a86291f1
aogutiancheng <18701987235@163.com>
2019-07-10 23:55:52 +0800

    add topic232 by chenkui

---
+++ chenkui/topic232_用栈实现队列/from chenkui.java
@@ -0,0 +1,38 @@
+﻿class MyQueue {
+
+    Stack<Integer> stack;
+    public MyQueue() {
+        stack=new Stack<Integer>();
+    }
+    public void push(int x) {
+        Stack<Integer> stackAuxi = new Stack<Integer>();
+        while(!stack.empty()) {
+            stackAuxi.push(stack.pop());
+        }
+        stack.push(x);
+        while(!stackAuxi.empty()) {
+            stack.push(stackAuxi.pop());
+        }
+    }
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return stack.pop(); 
+    }
+    /** Get the front element. */
+    public int peek() {
+        return stack.peek();
+    }
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack.empty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
