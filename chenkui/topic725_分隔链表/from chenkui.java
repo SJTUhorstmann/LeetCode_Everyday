4cdb7679b8504536c97accea1db6c3052b937cf4
aogutiancheng <18701987235@163.com>
2019-07-10 22:59:43 +0800

    add topic725 by chenkui

---
+++ chenkui/topic725_分隔链表/from chenkui.java
@@ -0,0 +1,37 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+     public ListNode[] splitListToParts(ListNode root, int k) {
+        int count=0;
+        ListNode node=root;
+        while(node!=null){
+            count++;
+            node=node.next;
+        }
+        int tem=count/k;
+        int tem1=count%k;
+
+        ListNode[] list=new ListNode[k];
+        
+        for (int i = 0; root != null && i < k; i++) {
+           list[i] = root;
+           int curSize = tem + (tem1-- > 0 ? 1 : 0);
+           for (int j = 0; j < curSize - 1; j++) {
+              root = root.next;
+           }
+           ListNode next = root.next;
+           root.next = null;
+           root = next;
+        }
+        
+        return list;
+    }
+    
+}
