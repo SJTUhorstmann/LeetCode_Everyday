a30873f995d806538df220c655218936819b8bad
aogutiancheng <18701987235@163.com>
2019-07-13 15:20:33 +0800

    add topic155 by chenkui

---
+++ chenkui/topic155_最小栈/from chenkui.java
@@ -0,0 +1,40 @@
+﻿class MinStack {
+     Stack<Integer> stack;
+     Stack<Integer> min;
+    
+    public MinStack() {
+     stack = new Stack<Integer>();
+     min=new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+      stack.push(x);
+      if(min.isEmpty()){
+          min.push(x);
+      }
+      else if(min.peek()>=x){
+          min.push(x);    
+      }
+    }
+    public void pop() {
+      int pop=stack.pop();
+      if(!min.isEmpty() && pop==min.peek()){
+          min.pop();
+      }
+    }
+    public int top() {
+        return stack.peek();
+    }
+    public int getMin() {
+        return min.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
