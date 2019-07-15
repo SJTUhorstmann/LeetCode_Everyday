c3ca710a49eab39eb0ccbd9c484789f053e8e940
aogutiancheng <18701987235@163.com>
2019-07-15 22:26:25 +0800

    add topic503 by chenkui

---
+++ chenkui/topic503_循环数组中比当前元素大的下一个元素/from chenkui.java
@@ -0,0 +1,52 @@
+﻿class Solution {
+    
+    //方法1
+    public int[] nextGreaterElements(int[] nums) {
+        int len=nums.length;
+        int[] result=new int[len];
+        Arrays.fill(result,-1);
+        
+        for(int i=0;i<len;i++){
+            int tem=nums[i];
+            boolean judge=false;
+            
+            for(int j=i+1;j<len;j++){
+                if(tem<nums[j]){
+                    result[i]=nums[j];
+                    judge=true;
+                    break;    
+                }
+            }
+            if(!judge){
+                for(int k=0;k<i;k++){
+                   if(tem<nums[k]){
+                      result[i]=nums[k];
+                      break;
+                   }
+                }  
+            }
+        }
+        
+        return result;
+        
+    }
+
+   //方法2
+   /* public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] next = new int[n];
+        Arrays.fill(next, -1);
+        Stack<Integer> pre = new Stack<>();
+        
+        for (int i = 0; i < n * 2; i++) {
+           int num = nums[i % n];
+           while (!pre.isEmpty() && nums[pre.peek()] < num) {
+              next[pre.pop()] = num;
+           }
+           if (i < n){
+              pre.push(i);
+           }
+        }
+       return next;
+}*/
+}
