d94717e8dcd0211c89f1c936989cb428b6ef4b9f
aogutiancheng <18701987235@163.com>
2019-07-15 21:53:36 +0800

    add topic739 by chenkui

---
+++ chenkui/topic739_数组中元素与下一个比它大的元素之间的距离/from chenkui.java
@@ -0,0 +1,32 @@
+﻿class Solution {
+    // 方法1
+    /*public int[] dailyTemperatures(int[] T) {
+        int[] result=new int[T.length];
+        for(int i=0;i<T.length;i++){
+            int tem=T[i];
+            for(int j=i+1;j<T.length;j++){
+                if(T[j]>tem){
+                    result[i]=j-i;
+                    break;
+                }
+            }
+        }
+        return result; 
+    }*/
+    
+    // 方法2
+    public int[] dailyTemperatures(int[] T) {
+    int[] result=new int[T.length];
+        
+    Stack<Integer> indexs = new Stack<>();
+    for (int curIndex = 0; curIndex <T.length; curIndex++) {
+        while (!indexs.isEmpty() && T[curIndex] > T[indexs.peek()]) {
+            int preIndex = indexs.pop();
+            result[preIndex] = curIndex - preIndex;
+        }
+        indexs.add(curIndex);
+    }
+    return result;
+}
+    
+}
