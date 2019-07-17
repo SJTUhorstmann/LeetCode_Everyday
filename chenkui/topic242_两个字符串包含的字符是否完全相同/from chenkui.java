b84790cc6443a3aa85b3f92500c063fce9359d3c
aogutiancheng <18701987235@163.com>
2019-07-17 23:39:55 +0800

    add topic242 by chenkui

---
+++ chenkui/topic242_两个字符串包含的字符是否完全相同/from chenkui.java
@@ -0,0 +1,31 @@
+﻿class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s==null && t==null){
+            return true;
+        }
+        else if(s==null || t==null){
+            return false;
+        }
+        else{
+            
+            int len1=s.length();
+            int len2=t.length();
+            if(len1!=len2){
+                return false;
+            }
+            int[] tem=new int[26];
+            for(int i=0;i<len1;i++){
+                int tem1=s.charAt(i)-'a';
+                int tem2=t.charAt(i)-'a';
+                tem[tem1]++;
+                tem[tem2]--;
+            }
+            for(int j=0;j<26;j++){
+                if(tem[j]!=0){
+                    return false;
+                }
+            }
+         }
+        return true;
+    }
+}
---
+++ chenkui/topic242_两个字符串包含的字符是否完全相同/readme.txt
@@ -0,0 +1,11 @@
+﻿题目描述：两个字符串包含的字符是否完全相同。
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
