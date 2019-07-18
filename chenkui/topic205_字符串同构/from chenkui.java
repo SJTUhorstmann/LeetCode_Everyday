d9db088efe671e7a3c8c484c1bdec9fc683cb86d
aogutiancheng <18701987235@163.com>
2019-07-18 23:39:13 +0800

    add topic205 by chenkui

---
+++ chenkui/topic205_字符串同构/from chenkui.java
@@ -0,0 +1,22 @@
+﻿class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int len1=s.length();
+        int len2=t.length();
+        if(len1!=len2){
+            return false;
+        }
+        
+        int[] tem1 = new int[256];
+        int[] tem2 = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+            if (tem1[sc] != tem2[tc]) {
+                return false;
+            }
+            tem1[sc] = i + 1;
+            tem2[tc] = i + 1;
+        }
+        return true;
+}
+}
---
+++ chenkui/topic205_字符串同构/readme.txt
@@ -0,0 +1,10 @@
+﻿题目描述：给定两个字符串s和t，确定它们是否是同构的。如果s中的字符可以替换为t，则两个字符串是同构的。
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
