d9cb48a6719af70c507d4306aad5648180ed6589
aogutiancheng <18701987235@163.com>
2019-07-20 11:55:37 +0800

    add topic9 by chenkui

---
+++ chenkui/topic09_回文数字/from chenkui1.java
@@ -0,0 +1,50 @@
+﻿class Solution {
+    
+    // 方法1
+   /* public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        String st="";
+        st=st+x;
+        int left=0;
+        int right=st.length()-1;
+        while(left<right){
+            char tem=st.charAt(left);
+            char tem1=st.charAt(right);
+            if(tem!=tem1){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+        
+    }*/
+    
+    
+    //方法2
+    public boolean isPalindrome(int x) {
+        int a=0;
+        int b=x;
+        if(x==0){
+            return true; 
+        }
+        else if(x>0){
+            while(x!=0){
+               a=a*10+x%10;
+               x=x/10;
+            }
+            if(a==b){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
