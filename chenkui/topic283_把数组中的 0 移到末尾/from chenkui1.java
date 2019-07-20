3568419e6dbc7a39b4af19dab60b0da1d186ddba
aogutiancheng <18701987235@163.com>
2019-07-20 23:29:09 +0800

    add topic283 by chenkui

---
+++ chenkui/topic283_把数组中的 0 移到末尾/from chenkui1.java
@@ -0,0 +1,17 @@
+﻿class Solution {
+    public void moveZeroes(int[] nums) {
+      if(nums==null || nums.length<2){
+           return;
+      } 
+      int j=0;
+      int tem;
+      for(int i=0;i<nums.length;i++){
+        if(nums[i]!=0){
+          tem=nums[j];
+          nums[j]=nums[i];
+          nums[i]=tem;
+          j++;
+        }
+      }
+    }
+}
