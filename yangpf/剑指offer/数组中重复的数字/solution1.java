70ceb61c7c66d64b1b95af1e7466d906a2889214
Yang <yangpf0810@sjtu.edu.cn>
2019-07-21 19:39:40 +0800

    数组中重复的数字

---
+++ yangpf/剑指offer/数组中重复的数字/solution1.java
@@ -0,0 +1,26 @@
+public class Solution {
+    // Parameters:
+    //    numbers:     an array of integers
+    //    length:      the length of array numbers
+    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        if(length==0){
+            duplication[0]=-1;
+            return false;
+        }
+        boolean[] arr=new boolean[numbers.length];
+        for(int i=0;i<numbers.length;i++){
+            if(arr[numbers[i]]==true){
+                duplication[0]=numbers[i];
+                return true;
+            }
+            arr[numbers[i]]=true;
+        }
+        duplication[0]=-1;
+        return false;
+    }
+}
