7237c888eaf2de5327d582ad6c819f5d482b2d45
aogutiancheng <18701987235@163.com>
2019-07-21 10:42:06 +0800

    add topic378 by chenkui

---
+++ chenkui/topic378_有序矩阵的第k小的元素/from chenkui.java
@@ -0,0 +1,36 @@
+﻿class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        if(matrix.length==1){
+            return matrix[0][0];
+        }
+        
+        int len=matrix.length*matrix[0].length;
+        if(len>2*k){
+            PriorityQueue<Integer> max=new PriorityQueue<Integer>((a,b)->b-a);
+            for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                max.add(matrix[i][j]);
+                if(max.size()>k){
+                    max.poll();
+                }
+            }
+        }
+        return max.peek();
+            
+        }
+        else{
+                    
+            PriorityQueue<Integer> min=new PriorityQueue<Integer>();
+            for(int i=0;i<matrix.length;i++){
+                for(int j=0;j<matrix[0].length;j++){
+                    min.add(matrix[i][j]);
+                    if(min.size()>(len-k+1)){
+                        min.poll();
+                    }
+                }
+            }
+            return min.peek();
+            
+      }
+    }
+}
