9d9fa2f720118789e8d1369a35ddebadad01d0c1
aogutiancheng <18701987235@163.com>
2019-07-24 21:32:54 +0800

    add topic268 by chenkui

---
+++ chenkui/topic268_找出数组中缺失的那个数/from chenkui.java
@@ -0,0 +1,25 @@
+﻿class Solution {
+    
+    //方法1
+    /*
+    public int missingNumber(int[] nums) {
+        int ret = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ret = ret ^ i ^ nums[i];
+        }
+        return ret ^ nums.length;
+    }
+    */
+    
+    //方法2
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=i){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+    
+}
