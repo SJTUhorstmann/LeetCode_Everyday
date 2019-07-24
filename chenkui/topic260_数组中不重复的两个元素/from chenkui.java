4dd42e26a8464b34ed3fea1139db24856ffa1b00
aogutiancheng <18701987235@163.com>
2019-07-24 21:42:47 +0800

    add topic260 by chenkui

---
+++ chenkui/topic260_数组中不重复的两个元素/from chenkui.java
@@ -0,0 +1,21 @@
+﻿class Solution {
+    public int[] singleNumber(int[] nums) {
+        int diff = 0;
+        for (int num : nums){
+            diff = diff ^ num;
+        } 
+        
+        diff = diff & -diff; 
+        int[] ret = new int[2];
+        
+        for (int num : nums) {
+            if ((num & diff) == 0) {
+                ret[0] =ret[0] ^ num;
+            }
+            else{
+                ret[1] =ret[1] ^ num;  
+            } 
+        }
+        return ret;
+    }
+}
