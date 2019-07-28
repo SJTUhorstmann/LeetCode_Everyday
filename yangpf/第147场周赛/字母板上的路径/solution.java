50b86e2b47ede0acef3863ae3a814e48e66ee55a
Yang <yangpf0810@sjtu.edu.cn>
2019-07-28 21:44:46 +0800

    字母板上的路径 修改解法

--- yangpf/第147场周赛/字母板上的路径/solution.java
+++ yangpf/第147场周赛/字母板上的路径/solution.java
@@ -5,58 +5,37 @@ class Solution {
         int preX=0,preY=0;
         for(int i=0;i<target.length();i++){
             char ch=target.charAt(i);
-            if(preX==5 && preY==0){
-                int x=(ch-'a')/5;
-                int y=(ch-'a')%5;
-                int offsetX=x-preX;
-                int offsetY=y-preY;
-                preX=x;
-                preY=y;
-                if(offsetX<0){
-                while(offsetX++<0){
-                    sb.append('U');
-                }
-                }else if(offsetX>0){
-                    while(x-->0){
-                        sb.append('D');
-                    }
-                }
-                if(offsetY<0){
-                    while(offsetY++<0){
-                        sb.append('L');
-                    }
-                }else if(offsetY>0){
-                    while(offsetY-->0){
-                        sb.append('R');
-                    }
-                }
-                sb.append('!');
-                continue;
-            }
             int x=(ch-'a')/5;
             int y=(ch-'a')%5;
             int offsetX=x-preX;
             int offsetY=y-preY;
-            preX=x;
-            preY=y;
-            if(offsetY<0){
-                while(offsetY++<0){
-                    sb.append('L');
-                }
-            }else if(offsetY>0){
-                while(offsetY-->0){
-                    sb.append('R');
-                }
-            }
-            if(offsetX<0){
+            if(preX==5 && preY==0){
                 while(offsetX++<0){
                     sb.append('U');
                 }
-            }else if(offsetX>0){
-                while(offsetX-->0){
+                while(offsetY-->0){
+                    sb.append('R');
+                }
+            }else{
+                while(offsetY<0){
+                    offsetY++;
+                    sb.append('L');
+                }
+                while(offsetY>0){
+                    offsetY--;
+                    sb.append('R');
+                }
+                while(offsetX<0){
+                    offsetX++;
+                    sb.append('U');
+                }
+                while(offsetX>0){
+                    offsetX--;
                     sb.append('D');
                 }
             }
+            preX=x;
+            preY=y;
             sb.append('!');
         }
         return sb.toString();
