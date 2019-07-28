ecb5dd1bb50ab7663980d5bdc1ce6b5b85307eeb
aogutiancheng <18701987235@163.com>
2019-07-28 11:49:24 +0800

    add topic5139 by chenkui

---
+++ chenkui/topic5139_第 N 个泰波那契数/from chenkui.java
@@ -0,0 +1,25 @@
+﻿class Solution {
+    public int tribonacci(int n) {
+        if(n==0){
+            return 0;
+        }
+        else if(n==1 || n==2){
+            return 1;
+        }
+
+        int first=0;
+        int second=1;
+        int third=1;
+        int result=2;
+
+        for(int i=3;i<n;i++){
+            first=second;
+            second=third;
+            third=result;
+            result=first+second+third;
+        }
+
+        return result;
+        
+    }
+}
