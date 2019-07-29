2b94b82ad43ebc46f94924e362eccc01f1af90df
aogutiancheng <18701987235@163.com>
2019-07-29 23:43:49 +0800

    add topic210 by chenkui

---
+++ chenkui/topic210_课程安排的合法性II/from chenkui.java
@@ -0,0 +1,49 @@
+﻿class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<Integer>[] graphic = new List[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graphic[i] = new ArrayList<>();
+        }
+        for (int[] pre : prerequisites) {
+            graphic[pre[0]].add(pre[1]);
+        }
+        
+        Stack<Integer> postOrder = new Stack<>();
+        boolean[] globalMarked = new boolean[numCourses];
+        boolean[] localMarked = new boolean[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (hasCycle(globalMarked, localMarked, graphic, i, postOrder)) {
+                return new int[0];
+            }
+        }
+        
+        int[] orders = new int[numCourses];
+        for (int i = numCourses - 1; i >= 0; i--) {
+            orders[i] = postOrder.pop();
+        }
+        return orders;
+    }
+
+    private boolean hasCycle(boolean[] globalMarked, boolean[] localMarked, List<Integer>[] graphic,
+        int curNode, Stack<Integer> postOrder) {
+
+        if (localMarked[curNode]) {
+            return true;
+        }
+        if (globalMarked[curNode]) {
+            return false;
+        }
+        
+        globalMarked[curNode] = true;
+        localMarked[curNode] = true;
+        for (int nextNode : graphic[curNode]) {
+            if (hasCycle(globalMarked, localMarked, graphic, nextNode, postOrder)) {
+                return true;
+            }
+        }
+        
+        localMarked[curNode] = false;
+        postOrder.push(curNode);
+        return false;
+    }
+    }
