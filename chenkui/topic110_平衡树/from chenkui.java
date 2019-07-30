a75e77bf7cf0b2e4f49fd94e6799350255617d9d
aogutiancheng <18701987235@163.com>
2019-07-30 22:11:00 +0800

    add topic110 by chenkui

---
+++ chenkui/topic110_平衡树/from chenkui.java
@@ -0,0 +1,68 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+//方法1
+
+/*
+class Solution {
+    boolean judge=true;
+    public boolean isBalanced(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        int len1=length(root.left);
+        int len2=length(root.right);
+        int tem=Math.abs(len1-len2);
+        if(tem>1){
+            return false;
+        }
+        else{
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+        
+    }
+    
+    private int length(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        else{
+            int left=length(root.left);
+            int right=length(root.right);
+            return Math.max(left,right)+1;
+        }
+    }
+}
+*/
+
+//方法2
+
+class Solution {
+    boolean result=true;
+    public boolean isBalanced(TreeNode root) {
+      deep(root);
+      return result;
+    }
+    
+    private int deep(TreeNode tree){
+        
+      if(tree==null){
+          return 0;
+      }
+      int left=deep(tree.left);
+      int right=deep(tree.right);
+        
+      if(Math.abs(left-right)>1){
+          result=false;
+      } 
+        
+      return 1+Math.max(left,right);
+    }
+}
