39a0ca172db0622994d191ccaffac4857ecce54a
Yang <yangpf0810@sjtu.edu.cn>
2019-07-31 21:55:13 +0800

    add topic27

---
+++ yangpf/topic27_移除元素/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==val){
+                continue;
+            }
+            nums[index++]=nums[i];
+        }
+        return index;
+    }
+}
