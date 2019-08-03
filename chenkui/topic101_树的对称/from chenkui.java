28e0944df9f743fd433d6de363d071b0932aeab1
aogutiancheng <18701987235@163.com>
2019-08-03 21:41:17 +0800

    add topic101 by chenkui

---
+++ chenkui/topic101_树的对称/from chenkui.java
@@ -0,0 +1,35 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        else{
+           return symmetric(root.left,root.right);
+        }
+        
+    }
+    
+    private boolean symmetric(TreeNode left,TreeNode right){
+        if(left==null && right==null){
+            return true;
+        }
+        else if(left==null || right==null){
+            return false;
+        }
+        else if(left.val != right.val){
+            return false;
+        }
+        else{
+            return symmetric(left.left,right.right) && symmetric(left.right,right.left);
+        }
+    }
+}
