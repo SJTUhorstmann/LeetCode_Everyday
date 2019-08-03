f9f355dd6e3415f07ab655d083b9b548ab84f748
aogutiancheng <18701987235@163.com>
2019-08-03 22:20:06 +0800

    add topic404 by chenkui

---
+++ chenkui/topic404_统计左叶子节点的和/from chenkui.java
@@ -0,0 +1,23 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public int sumOfLeftLeaves(TreeNode root) {
+        int sum=0;
+        if(root==null){
+            return 0;
+        }
+        if(root.left!=null && root.left.left==null && root.left.right==null){
+            sum=sum+root.left.val;
+        }
+
+        return sum+sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
+    }
+}
