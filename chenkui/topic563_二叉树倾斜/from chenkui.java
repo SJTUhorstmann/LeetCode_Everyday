330699b18d671a868f8b732ecbf9b21ce09078ae
aogutiancheng <18701987235@163.com>
2019-08-04 01:07:15 +0800

    add topic563 by chenkui

---
+++ chenkui/topic563_二叉树倾斜/from chenkui.java
@@ -0,0 +1,28 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int result=0;
+    public int findTilt(TreeNode root) {
+      if(root==null) {
+          return 0;
+      }
+      Tilt(root);
+      return result;
+    }
+    private int Tilt(TreeNode root){
+      if(root==null) {
+          return 0;
+      }
+      int left=Tilt(root.left);
+      int right=Tilt(root.right);
+      result=result+Math.abs(left-right);
+      return left+right+root.val;
+    }
+}
