419fb10bcbd4059830435a6755aa17f325b751ac
aogutiancheng <18701987235@163.com>
2019-08-09 00:24:08 +0800

    add topic538 by chenkui

---
+++ chenkui/topic538_把二叉查找树每个节点的值都加上比它大的节点的值/from chenkui.java
@@ -0,0 +1,23 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    int sum=0;
+    public TreeNode convertBST(TreeNode root) {
+      if(root==null) {
+          return null;
+      }
+      convertBST(root.right);
+      sum=sum+root.val;
+      root.val=sum;
+      convertBST(root.left);
+      return root;  
+    }
+}
