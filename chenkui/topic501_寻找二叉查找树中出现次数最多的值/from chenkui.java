254923c7002a1e900bc4547b725d0a8c500f921f
aogutiancheng <18701987235@163.com>
2019-08-10 12:25:46 +0800

    add topic501 by chenkui

---
+++ chenkui/topic501_寻找二叉查找树中出现次数最多的值/from chenkui.java
@@ -0,0 +1,47 @@
+﻿ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+private int curCnt = 1;
+private int maxCnt = 1;
+private TreeNode preNode = null;
+
+public int[] findMode(TreeNode root) {
+    List<Integer> maxCntNums = new ArrayList<>();
+    inOrder(root, maxCntNums);
+    int[] ret = new int[maxCntNums.size()];
+    int idx = 0;
+    
+    for (int num : maxCntNums) {
+        ret[idx++] = num;
+    }
+    
+    return ret;
+}
+
+private void inOrder(TreeNode node, List<Integer> nums) {
+    if (node == null){
+        return;
+    } 
+    inOrder(node.left, nums);
+    if (preNode != null) {
+        if (preNode.val == node.val) curCnt++;
+        else curCnt = 1;
+    }
+    if (curCnt > maxCnt) {
+        maxCnt = curCnt;
+        nums.clear();
+        nums.add(node.val);
+    } 
+    else if (curCnt == maxCnt) {
+        nums.add(node.val);
+    }
+    
+    preNode = node;
+    inOrder(node.right, nums);
+}
+}
