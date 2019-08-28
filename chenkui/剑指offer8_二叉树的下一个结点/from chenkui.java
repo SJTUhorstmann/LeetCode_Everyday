ca69d566f9ad503c6618174bb72d7b24347ed8f8
aogutiancheng <18701987235@163.com>
2019-08-28 23:41:22 +0800

    add 剑指offer8 by chenkui

---
+++ chenkui/剑指offer8_二叉树的下一个结点/from chenkui.java
@@ -0,0 +1,39 @@
+﻿/*
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
+*/
+
+public class Solution {
+    public TreeLinkNode GetNext(TreeLinkNode pNode)
+    {
+        if(pNode==null){
+            return null;
+        } 
+        if(pNode.right!=null){
+            pNode=pNode.right;
+            while(pNode.left!=null){
+                pNode=pNode.left;
+            }
+            return pNode;
+        }
+        TreeLinkNode node=pNode.next;
+        while(node!=null){
+            if(node.left==pNode){
+                return node;
+            } 
+            else {
+                pNode=node;
+                node=node.next;
+            }
+        }
+        return null;
+    }
+}
