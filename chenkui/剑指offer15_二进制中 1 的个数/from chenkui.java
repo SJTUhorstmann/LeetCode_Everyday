07af65c82073dacfbfc9dd37f2f47ac3d316d3cb
aogutiancheng <18701987235@163.com>
2019-08-30 00:24:16 +0800

    add 剑指offer15 by chenkui

---
+++ chenkui/剑指offer15_二进制中 1 的个数/from chenkui.java
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public int NumberOf1(int n) {
+        if(n==0){
+            return 0;
+        }
+        int count=0;
+        while(n!=0){
+            count++;
+            n=n&(n-1);
+        }
+         return count;
+    }
+}
