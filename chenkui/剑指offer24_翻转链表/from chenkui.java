9f43a52520ffdf86aecada4ec8708be9926be290
aogutiancheng <18701987235@163.com>
2019-08-31 23:55:05 +0800

    add 剑指offer24 by chenkui

---
+++ chenkui/剑指offer24_翻转链表/from chenkui.java
@@ -0,0 +1,41 @@
+﻿/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+
+/*
+//递归
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode next=head.next;
+        ListNode reverse=ReverseList(next);
+        head.next=null;
+        next.next=head;
+        return reverse;
+    }
+}*/
+
+//迭代
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode result=new ListNode(0);
+        while(head!=null){
+            ListNode next=head.next;
+            head.next=result.next;
+            result.next=head;
+            head=next;
+        }
+        return result.next;
+    }
+}
