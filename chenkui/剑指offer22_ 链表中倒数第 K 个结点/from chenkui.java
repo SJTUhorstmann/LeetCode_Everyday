d2f823e2e66e012e20ede45726cd818872b28412
aogutiancheng <18701987235@163.com>
2019-08-31 23:35:00 +0800

    add 剑指offer22 by chenkui

---
+++ chenkui/剑指offer22_ 链表中倒数第 K 个结点/from chenkui.java
@@ -0,0 +1,29 @@
+﻿/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode FindKthToTail(ListNode head, int k) {
+    if (head == null){
+        return null;
+    } 
+    ListNode P1 = head;
+    while (P1 != null && k-- > 0){
+        P1 = P1.next;
+    } 
+    if (k > 0){
+        return null;
+    } 
+    ListNode P2 = head;
+    while (P1 != null) {
+        P1 = P1.next;
+        P2 = P2.next;
+    }
+    return P2;
+}
+}
