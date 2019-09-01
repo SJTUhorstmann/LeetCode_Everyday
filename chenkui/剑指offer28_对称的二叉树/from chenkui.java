392b65f0099f42e2bc388d5c5debda38d104002c
aogutiancheng <18701987235@163.com>
2019-09-01 12:20:32 +0800

    add 剑指offer28 by chenkui

---
+++ chenkui/剑指offer28_对称的二叉树/from chenkui.java
@@ -0,0 +1,36 @@
+﻿/*
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+
+public class Solution {
+    boolean isSymmetrical(TreeNode pRoot)
+    {
+        if(pRoot==null || (pRoot.left==null && pRoot.right==null)){
+            return true;
+        }
+        return judge(pRoot.left,pRoot.right);
+    }
+    
+    public boolean judge(TreeNode left,TreeNode right){
+        if(left==null && right==null){
+            return true;
+        }
+        if(left==null || right==null) {
+            return false;
+        }
+        if(left.val!=right.val){
+            return false;
+        }
+        return judge(left.left,right.right) && judge(left.right,right.left);
+    }
+}
