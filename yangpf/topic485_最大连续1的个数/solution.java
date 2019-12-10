652af8571993bc2c379b7293f9f6d055c2bad9c0
Yang <yangpf0810@sjtu.edu.cn>
2019-12-10 14:38:44 +0800

    topic485

---
+++ yangpf/topic485_最大连续1的个数/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0;
+        int count=0;
+        for(int i:nums){
+            if(i==1)    count++;
+            else    count=0;
+            max=Math.max(max,count);
+        }
+        return max;
+    }
+}
