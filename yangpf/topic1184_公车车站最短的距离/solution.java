e4785c2983ebb4dff1899bc241e83ef825735865
Yang <yangpf0810@sjtu.edu.cn>
2019-12-23 13:26:30 +0800

    add solution for topic1184

--- yangpf/topic1184_公车车站最短的距离/solution.java
+++ yangpf/topic1184_公车车站最短的距离/solution.java
@@ -1,3 +1,4 @@
+//33%
 class Solution {
     public int distanceBetweenBusStops(int[] distance, int start, int destination) {
         int sum=0;
@@ -6,6 +7,23 @@ class Solution {
             sum+=i;
         }
         for(int i=Math.min(start,destination);i<Math.max(start,destination);i++){
+            dis+=distance[i];
+        }
+        return Math.min(dis,sum-dis);
+    }
+}
+
+// 100%
+class Solution {
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        int sum=0;
+        int dis=0;
+        for(int i:distance){
+            sum+=i;
+        }
+        int l=Math.min(start,destination);
+        int r=Math.max(start,destination);
+        for(int i=l;i<r;i++){
             dis+=distance[i];
         }
         return Math.min(dis,sum-dis);
