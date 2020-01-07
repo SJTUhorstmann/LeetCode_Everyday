74641a4ef343ec38bffb06801a3850e4da7c9aed
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-01-07 12:00:45 +0800

    commit wanjunlin/suanfa001_三数之和 file

---
+++ wanjunlin/suanfa001_三数之和/数组排序 双指针 O(N2).java
@@ -0,0 +1,25 @@
+public class ThreeSumTwoPointer implements ThreeSum {
+
+    @Override
+    public int count(int[] nums) {
+        int N = nums.length;
+        int cnt = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < N - 2; i++) {
+            int l = i + 1, h = N - 1, target = -nums[i];
+            while (l < h) {
+                int sum = nums[l] + nums[h];
+                if (sum == target) {
+                    cnt++;
+                    l++;
+                    h--;
+                } else if (sum < target) {
+                    l++;
+                } else {
+                    h--;
+                }
+            }
+        }
+        return cnt;
+    }
+}
