9173b5371183affb36b72d54cd8b0135503a9d2a
Yang <yangpf0810@sjtu.edu.cn>
2020-01-09 15:54:11 +0800

    add new solution for 821

--- yangpf/topic821_字符的最短距离/solution.java
+++ yangpf/topic821_字符的最短距离/solution.java
@@ -1,3 +1,23 @@
+//时间复杂度为N的解法，两次遍历，从左往右和从右向左
+class Solution {
+    public int[] shortestToChar(String S, char C) {
+        int len=S.length();
+        int[] arr=new int[len];
+        //好好理解一下pre和post的初始赋值，为何这么赋值
+        int pre=Integer.MIN_VALUE/2;
+        for(int i=0;i<len;i++){
+            if(S.charAt(i)==C)  pre=i;
+            arr[i]=i-pre;
+        }
+        int post=Integer.MAX_VALUE/2;
+        for(int i=len-1;i>=0;i--){
+            if(S.charAt(i)==C)  post=i;
+            arr[i]=Math.min(arr[i],post-i);
+        }
+        return arr;
+    }
+}
+
 //从每个字符向两边扩散寻找字符C
 class Solution {
     public int[] shortestToChar(String S, char C) {
