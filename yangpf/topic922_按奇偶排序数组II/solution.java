99d01ef3fb6c039ed919b75c545e34b11d948849
Yang <yangpf0810@sjtu.edu.cn>
2020-01-12 14:26:37 +0800

    add solution for topic922

--- yangpf/topic922_按奇偶排序数组II/solution.java
+++ yangpf/topic922_按奇偶排序数组II/solution.java
@@ -17,3 +17,21 @@ class Solution {
         return res;
     }
 }
+
+//不适用额外空间，交换位置
+class Solution {
+    public int[] sortArrayByParityII(int[] A) {
+        int j=1;
+        for(int i=0;i<A.length;i=i+2){
+            if(A[i]%2==1){
+                while(j<A.length && A[j]%2==1){
+                    j=j+2;
+                }
+                int tmp=A[j];
+                A[j]=A[i];
+                A[i]=tmp;
+            }
+        }
+        return A;
+    }
+}
