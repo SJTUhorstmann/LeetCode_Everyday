98455a90ec2545155a22452e6e399b37f8def506
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-01-14 17:19:51 +0800

    commit wanjunlin/lin005_队列和广度优先搜索 file

---
+++ wanjunlin/lin005_队列和广度优先搜索/广度优先搜索模板2.java
@@ -0,0 +1,34 @@
+/**
+ * Return the length of the shortest path between root and target node.
+ */
+int BFS(Node root, Node target) {
+    Queue<Node> queue;  // store all nodes which are waiting to be processed
+    Set<Node> used;     // store all the used nodes
+    int step = 0;       // number of steps neeeded from root to current node
+    // initialize
+    add root to queue;
+    add root to used;
+    // BFS
+    while (queue is not empty) {
+        step = step + 1;
+        // iterate the nodes which are already in the queue
+        int size = queue.size();
+        for (int i = 0; i < size; ++i) {
+            Node cur = the first node in queue;
+            return step if cur is target;
+            for (Node next : the neighbors of cur) {
+                if (next is not in used) {
+                    add next to queue;
+                    add next to used;
+                }
+            }
+            remove the first node from queue;
+        }
+    }
+    return -1;          // there is no path from root to target
+}
+
+//有两种情况你不需要使用哈希集：
+
+//1.你完全确定没有循环，例如，在树遍历中；
+//2.你确实希望多次将结点添加到队列中。
